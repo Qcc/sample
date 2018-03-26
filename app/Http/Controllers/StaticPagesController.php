9fcac0af9cfd50a09857c8af6f1c9e467e121a7f
kevin <kevin.e@qq.com>
2018-03-26 01:28:09 +0000

    database mirgiate

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    //
+    public function home()
+    {
+    	return 'home';
+    }
+
+    public function about()
+    {
+    	return 'about';
+    }
+
+    public function help()
+    {
+    	return 'help';
+    }
 }
